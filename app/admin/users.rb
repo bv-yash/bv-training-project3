ced1a9527d1234b1c48e843455b9d756b3540233
bv-yash <yash@blogvault.net>
2021-06-11 13:30:43 +0530

    updates in active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register User do
-        permit_params :email
+        permit_params :email, :password, :password_confirmation,  :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+
+        filter :email
+        filter :id
+        show do
+                attributes_table do
+                        row :email
+                        row :created_at
+                        row :updated_at
+                end     
+        end
+
+        form do |f|
+                f.semantic_errors *f.object.errors.keys
+                inputs do
+                        input:email
+                        input:password
+                        input:password_confirmation
+                end
+                actions
+        end
 end
